741ff72f86e104cf120708ed6993b2f878cfa7b6
qbart <wojtowicz.bartlomiej@gmail.com>
2025-04-05 14:05:27 +0200

    feat: Errors

--- expecto/errors.go
+++ expecto/errors.go
@@ -21,8 +21,11 @@ func (s *Assert) Err(msg string, err error) {
 }
 
 func (s *Assert) ErrContains(msg string, err error, substr string) {
-	if err == nil || !strings.Contains(err.Error(), substr) {
+	s.Err(msg, err)
+
+	if !strings.Contains(err.Error(), substr) {
 		s.t.Logf("  %s:", msg)
+		s.t.Logf("    want: \033[0;32m%v\033[0m", substr)
 		s.t.Logf("     got: \033[0;31m%v\033[0m", err)
 		s.t.Fatalf("Expected error containing %s", substr)
 	}
